fea5d6a41c750f2d684b6115d92804da43ac9150
Ed <Siniakov>
2023-06-07 23:41:09 +0300

    Initial commit

--- EKBrowser/SceneDelegate.swift
+++ EKBrowser/SceneDelegate.swift
@@ -13,10 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+       createWindow(scene: scene)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -50,3 +47,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+extension SceneDelegate {
+    private func createWindow(scene: UIScene) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let nc = UINavigationController()
+        let viewController = EKBrowserViewController()
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
